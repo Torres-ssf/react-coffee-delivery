dfcf666bc596a64e6c019866047406c3cd3646d9
Torres-ssf <sergio.uft@gmail.com>
2022-07-25 15:47:56 -0300

    set up global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle, css } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: none;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme['yellow-500']};
+  }
+
+  body {
+    ${({ theme }) => css`
+      background: ${theme.background};
+      color: ${theme['gray-700']};
+    `};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`
