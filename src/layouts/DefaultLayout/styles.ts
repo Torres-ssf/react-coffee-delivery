f3081398a38e6c5cb50ef1c6b939296371eecb80
Torres-ssf <sergio.uft@gmail.com>
2022-08-01 11:43:59 -0300

    implement DefaultLayout component

---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const DefaultLayoutContainer = styled.div`
+  max-width: 90rem;
+  margin: 0 auto;
+  padding: 0 10rem;
+
+  @media (max-width: 1480px) {
+    padding: 0 5rem;
+  }
+  @media (max-width: 1366px) {
+    padding: 0 2rem;
+  }
+`
