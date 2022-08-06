05d0ac860af6986523c61d0168c4b25aa65dae7b
Torres-ssf <sergio.uft@gmail.com>
2022-08-06 09:21:58 -0300

    add route order confirmed

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
 import { Checkout } from './pages/Checkout'
 import { Home } from './pages/Home'
+import { OrderConfirmed } from './pages/OrderConfirmed'
 
 export function Router() {
   return (
@@ -9,6 +10,7 @@ export function Router() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/checkout" element={<Checkout />} />
+        <Route path="/order-confirmed" element={<OrderConfirmed />} />
       </Route>
     </Routes>
   )
