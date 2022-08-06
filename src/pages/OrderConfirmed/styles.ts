6a6246aa779a9b713afcb9dad528d9a82de9c39b
Torres-ssf <sergio.uft@gmail.com>
2022-08-06 09:21:31 -0300

    add styles for OrderConfirmed component

---
+++ src/pages/OrderConfirmed/styles.ts
@@ -0,0 +1,83 @@
+import styled from 'styled-components'
+
+export const OrderConfirmedContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+
+  margin-top: 5rem;
+
+  h1 {
+    color: ${({ theme }) => theme['yellow-500']};
+
+    font-family: 'Baloo 2', cursive;
+    font-weight: 800;
+    font-size: 2rem;
+    line-height: 2.6rem;
+  }
+
+  p {
+    color: ${({ theme }) => theme['gray-800']};
+    font-size: 1.25rem;
+    line-height: 1.625rem;
+  }
+`
+
+export const InnerContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+export const UserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
+  padding: 2.5rem;
+
+  background: linear-gradient(white, white) padding-box,
+    linear-gradient(to right, #dbac2c, #8047f8) border-box;
+  border-radius: 6px 36px;
+  border: 1px solid transparent;
+
+  margin-top: 2.5rem;
+  width: 32.875rem;
+`
+
+export const UserDetailsInfoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+
+    font-size: 1rem;
+    line-height: 1.3rem;
+    color: ${({ theme }) => theme['gray-700']};
+  }
+`
+
+export interface IconContainerProps {
+  iconColor: string
+}
+
+export const IconConatiner = styled.span<IconContainerProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 2rem;
+  height: 2rem;
+
+  border-radius: 999px;
+
+  background-color: ${({ iconColor }) => iconColor};
+`
+
+export const DeliveryIlustration = styled.img`
+  align-self: flex-end;
+  justify-self: flex-end;
+`
