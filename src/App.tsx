6c63fad16161490b0edd1002cd7d73447a0c239b
Torres-ssf <sergio.uft@gmail.com>
2022-08-08 15:09:30 -0300

    using CoffeeContextProvider on App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { CoffeeContextProvider } from './context/CoffeeContext'
 import { Router } from './Router'
 
 import { GlobalStyle } from './styles/global'
@@ -9,7 +10,9 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CoffeeContextProvider>
+          <Router />
+        </CoffeeContextProvider>
       </BrowserRouter>
 
       <GlobalStyle />
