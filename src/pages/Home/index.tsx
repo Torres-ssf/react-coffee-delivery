899d4ecca734dda769bc436bbb4e59121eeb2613
Torres-ssf <sergio.uft@gmail.com>
2022-08-08 15:10:09 -0300

    made Home component use data from coffee context

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,12 @@
-import { CoffeeItem, ICoffeeItemProps } from '../../components/CoffeeItem'
+import { CoffeeItem } from '../../components/CoffeeItem'
 import { IntroContent } from '../../components/IntroContent'
 import { CoffeeList, HomeContainer } from './styles'
 
-import db from '../../../db.json'
+import { useContext } from 'react'
+import { CoffeeContext } from '../../context/CoffeeContext'
 
 export function Home() {
-  const { coffees } = db
+  const { coffees } = useContext(CoffeeContext)
 
   return (
     <HomeContainer>
@@ -14,8 +15,8 @@ export function Home() {
       <h2>Our coffees</h2>
 
       <CoffeeList>
-        {coffees.map((coffee: ICoffeeItemProps) => (
-          <CoffeeItem key={coffee.name} {...coffee} />
+        {coffees.map(coffee => (
+          <CoffeeItem key={coffee.id} {...coffee} />
         ))}
       </CoffeeList>
     </HomeContainer>
