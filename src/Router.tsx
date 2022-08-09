7b5103fd66fb1bdcc79662587fbed4c8e5da0ac5
Torres-ssf <sergio.uft@gmail.com>
2022-08-09 19:39:23 -0300

    ensure paymentInfo exists before accessing order confimed path

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,16 +1,25 @@
-import { Routes, Route } from 'react-router-dom'
+import { useContext } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { CoffeeContext } from './context/CoffeeContext'
 import { DefaultLayout } from './layouts/DefaultLayout'
 import { Checkout } from './pages/Checkout'
 import { Home } from './pages/Home'
 import { OrderConfirmed } from './pages/OrderConfirmed'
 
 export function Router() {
+  const { paymentInfo } = useContext(CoffeeContext)
+
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/checkout" element={<Checkout />} />
-        <Route path="/order-confirmed" element={<OrderConfirmed />} />
+        <Route
+          path="/order-confirmed"
+          element={
+            paymentInfo ? <OrderConfirmed /> : <Navigate to="/checkout" />
+          }
+        />
       </Route>
     </Routes>
   )
